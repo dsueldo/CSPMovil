53630f1d10c5c45d280e12995bdfee9463f9bea1
Diego Sueldo <diego_sueldo5@hotmail.com>
2024-11-12 01:08:56 -0500

    feat: configure and test firebase crashlytics

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
     alias(libs.plugins.android.library) apply false
-    id("com.google.firebase.crashlytics") version "2.9.9" apply false
+    alias(libs.plugins.google.services) apply false
+    alias(libs.plugins.crashlytics) apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
 }
