6de9cb36097d0b5002b8e20b4f00ad8048232b53
Diego Sueldo <diego_sueldo5@hotmail.com>
2024-11-12 01:08:56 -0500

    feat: configure and test firebase crashlytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.gms.google-services")
-    id("com.google.firebase.crashlytics")
+    alias(libs.plugins.google.services)
+    alias(libs.plugins.crashlytics)
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
 }
--- app/src/main/java/com/remotecsolutionsperu/cspmovil/MainActivity.kt
+++ app/src/main/java/com/remotecsolutionsperu/cspmovil/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -40,6 +41,7 @@ fun PantallaLogo() {
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
+            .clickable { throw RuntimeException("Test Crash") }
     ) {
         Column(
             modifier = Modifier.fillMaxSize(),
