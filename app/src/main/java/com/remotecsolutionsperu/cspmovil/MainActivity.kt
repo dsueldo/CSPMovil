ba1399532d7e54d2e34a8ffae1af8ad1b9845085
Diego Sueldo <diego_sueldo5@hotmail.com>
2024-12-02 01:27:42 -0500

    feat: Add PaymentOne Screen and ItemPaymentComponent

--- app/src/main/java/com/remotecsolutionsperu/cspmovil/MainActivity.kt
+++ app/src/main/java/com/remotecsolutionsperu/cspmovil/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.remotecsolutionsperu.cspmovil.ui.theme.CSPMovilTheme
 import com.remotecsolutionsperu.presentation.ui.benefits.BenefitsScreen
 import com.remotecsolutionsperu.presentation.ui.main.MainScreen
+import com.remotecsolutionsperu.presentation.ui.payment.PaymentOneScreen
 
 class MainActivity : ComponentActivity() {
 
@@ -22,9 +23,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             CSPMovilTheme {
                 //MainScreen(feedList = listOf("Feed", "Mi Cuenta", "Beneficios", "Pagos"))
-                BenefitsScreen(benefitList = listOf(
+                /*BenefitsScreen(benefitList = listOf(
                     "benefit1", "benefit2", "benefit3", "benefit4", "benefit5", "benefit6", "benefit7", "benefit8", "benefit9", "benefit10"
-                ))
+                ))*/
+                PaymentOneScreen(paymentList = listOf("payment1", "payment2", "payment3","payment4","payment5","payment6","payment7","payment8","payment9","payment10"))
             }
         }
     }
---
+++ presentation/src/main/java/com/remotecsolutionsperu/presentation/ui/payment/PaymentOneScreen.kt
@@ -0,0 +1,40 @@
+package com.remotecsolutionsperu.presentation.ui.payment
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.remotecsolutionsperu.presentation.ui.components.ItemPaymentComponent
+import com.remotecsolutionsperu.presentation.ui.components.TitleComponent
+
+@Composable
+fun PaymentOneScreen(
+    paymentList: List<String>,
+    modifier: Modifier = Modifier
+) {
+
+    Column {
+        TitleComponent(text = "Pagos")
+        LazyColumn(modifier = modifier) {
+            items(paymentList) { payment ->
+                ItemPaymentComponent(
+                    image = "https://colegiodesociologosperu.org.pe/wp-content/uploads/2023/02/pronunciamiento-768x433.png",
+                    title = "Cuotas Sociales Ordinarias",
+                    content = "Contenido",
+                    onClick = {}
+                )
+            }
+        }
+    }
+
+}
+
+@Preview
+@Composable
+private fun PaymentOneScreenPreview() {
+    PaymentOneScreen(
+        listOf("payment1", "payment2", "payment3")
+    )
+}
