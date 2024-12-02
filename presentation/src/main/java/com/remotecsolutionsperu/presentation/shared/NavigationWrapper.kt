2af36eb2f67e01b43e6985ab3974b6ab1b0bc26f
Diego Sueldo <diego_sueldo5@hotmail.com>
2024-12-01 23:43:04 -0500

    feat: Implementation Login Screen

--- presentation/src/main/java/com/remotecsolutionsperu/presentation/shared/NavigationWrapper.kt
+++ presentation/src/main/java/com/remotecsolutionsperu/presentation/shared/NavigationWrapper.kt
@@ -1,29 +1,6 @@
 package com.remotecsolutionsperu.presentation.shared
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import com.remotecsolutionsperu.presentation.ui.splash.SplashScreen
 
 @Composable
-fun NavigationWrapper(
-    navHostController: NavHostController,
-) {
-
-    NavHost(navController = navHostController, startDestination = "splash") {
-        composable("splash") {
-            SplashScreen(/*navigateToLogin = { navHostController.navigate("logIn") },
-                navigateToSignUp = { navHostController.navigate("signUp") }*/)
-        }
-        composable("logIn") {
-            LoginScreen(auth){ navHostController.navigate("home") }
-        }
-        composable("signUp") {
-            SignUpScreen(auth)
-        }
-        composable("home"){
-            HomeScreen()
-        }
-    }
-}
+fun NavigationWrapper() {}
--- presentation/src/main/java/com/remotecsolutionsperu/presentation/ui/splash/SplashScreen.kt
+++ presentation/src/main/java/com/remotecsolutionsperu/presentation/ui/splash/SplashScreen.kt
@@ -1,4 +1,57 @@
 package com.remotecsolutionsperu.presentation.ui.splash
 
-class SplashScreen {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.remotecsolutionsperu.presentation.R
+
+@Composable
+fun SplashScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .clickable { throw RuntimeException("Test Crash") }
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            /*Image(
+                painter = painterResource(id = R.drawable.),
+                contentDescription = "CSP Móvil"
+            )*/
+            Text(
+                text = "Colegio de Sociólogos del Perú",
+                fontSize = 30.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                letterSpacing = 5.sp
+            )
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PantallaLogoPreview() {
+    SplashScreen()
 }
