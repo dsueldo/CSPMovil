399d6b1544589d697d52eaa20d9676e4ee474a0e
Diego Sueldo <diego_sueldo5@hotmail.com>
2025-03-19 01:25:49 -0500

    feat: Implement loading and states in newsList and benefitsList

--- presentation/src/main/java/com/remotecsolutionsperu/cspmovil/presentation/viewmodels/news/NewsListViewModel.kt
+++ presentation/src/main/java/com/remotecsolutionsperu/cspmovil/presentation/viewmodels/news/NewsListViewModel.kt
@@ -18,6 +18,15 @@ class NewsListViewModel @Inject constructor() : CspAppViewModel() {
     private val _newsList = MutableStateFlow<List<NewsItem>>(emptyList())
     val newsList: StateFlow<List<NewsItem>> = _newsList
 
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    private val _errorMessage = MutableStateFlow("")
+    val errorMessage: StateFlow<String> = _errorMessage
+
+    private val _uiState = MutableStateFlow(false)
+    val uiState: StateFlow<Boolean> = _uiState
+
     private val db = Firebase.firestore
 
     init {
@@ -25,32 +34,33 @@ class NewsListViewModel @Inject constructor() : CspAppViewModel() {
     }
 
     private fun fetchNewsList() {
+        _isLoading.value = true
         viewModelScope.launch {
             db.collection("news")
-                .addSnapshotListener { snapshot, e ->
-                    if (e != null) {
-                        Log.w(TAG, "Listen failed.", e)
-                        return@addSnapshotListener
-                    }
-
-                    if (snapshot != null) {
-                        val newsItems = snapshot.documents.mapNotNull { document ->
-                            val image = document.getString("image")
-                            val title = document.getString("title")
-                            val content = document.getString("content")
-                            Log.d(TAG, "Current data Image: $image")
-                            Log.d(TAG, "Current data Title: $title")
-                            Log.d(TAG, "Current data Content: $content")
-                            if (image != null && title != null && content != null) {
-                                NewsItem(image, title, content)
-                            } else {
-                                null
-                            }
+                .get()
+                .addOnSuccessListener { snapshot ->
+                    val newsItems = snapshot.documents.mapNotNull { document ->
+                        val image = document.getString("image")
+                        val title = document.getString("title")
+                        val content = document.getString("content")
+                        Log.d(TAG, "Current data Image: $image")
+                        Log.d(TAG, "Current data Title: $title")
+                        Log.d(TAG, "Current data Content: $content")
+                        if (image != null && title != null && content != null) {
+                            NewsItem(image, title, content)
+                        } else {
+                            null
                         }
-                        _newsList.value = newsItems
-                    } else {
-                        Log.d(TAG, "Current data: null")
                     }
+                    _newsList.value = newsItems
+                    _uiState.value = true
+                    _isLoading.value = false
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error fetching news", e)
+                    _errorMessage.value = "Error fetching news: ${e.message}"
+                    _uiState.value = false
+                    _isLoading.value = false
                 }
         }
     }
