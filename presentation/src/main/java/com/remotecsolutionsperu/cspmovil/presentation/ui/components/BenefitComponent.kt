0a67e7f343d8f47b6b5cce6acd90987444bdda55
Diego Sueldo <diego_sueldo5@hotmail.com>
2025-04-07 20:36:42 -0500

    fix: add space between texts

--- presentation/src/main/java/com/remotecsolutionsperu/cspmovil/presentation/ui/components/BenefitComponent.kt
+++ presentation/src/main/java/com/remotecsolutionsperu/cspmovil/presentation/ui/components/BenefitComponent.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
@@ -45,14 +46,16 @@ fun BenefitComponent(
                 text = title,
                 fontWeight = FontWeight.Bold,
                 style = Typography.bodyLarge,
-                color = Color.Black
+                color = Color.Black,
+                textAlign = TextAlign.Justify,
             )
             Spacer(modifier = Modifier.height(4.dp))
             Text(
                 modifier = Modifier.fillMaxWidth(),
                 text = content,
                 style = Typography.bodyMedium,
-                color = Color.Black
+                color = Color.Black,
+                textAlign = TextAlign.Justify,
             )
         }
     }
