936ec47e171656365abe917a437bf8e5455edd69
Diego Sueldo <diego_sueldo5@hotmail.com>
2025-05-03 20:46:18 -0500

    refactor: Implement more space for buttons

--- presentation/src/main/java/com/remotecsolutionsperu/cspmovil/presentation/ui/profile/ProfileFooter.kt
+++ presentation/src/main/java/com/remotecsolutionsperu/cspmovil/presentation/ui/profile/ProfileFooter.kt
@@ -6,14 +6,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.remotecsolutionsperu.cspmovil.presentation.utils.theme.Red_Dark
@@ -29,7 +26,7 @@ fun ProfileFooter(
         modifier = modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(32.dp))
         Button(
             onClick = onSignOut,
             colors = ButtonDefaults.textButtonColors(Red_Dark),
